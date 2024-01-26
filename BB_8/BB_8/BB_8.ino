6e1890a98d3cb0cb1dba3d67308d3906eb61722b
JuliusOrtstadt <120115242+JuliusOrtstadt@users.noreply.github.com>
2024-01-26 19:00:01 +0100

    Update BB_8.ino

--- BB_8/BB_8/BB_8.ino
+++ BB_8/BB_8/BB_8.ino
@@ -45,7 +45,7 @@ const int thresholdDiagonal = 600;
 const int thresholdSide = 400;
 
 // Variables for the speed of the robot.
-const int speed = 255;
+const int speed = 225;
 const int turnSpeed = speed - 100;
 
 // Variables for tile BB_8.detection.
@@ -174,7 +174,7 @@ void Robot::findBlackTile(){
     else { 
       BB_8.tileDetection();
       BB_8.backToStart();
-      Serial.println("back to star");
+      //Serial.println("Back to start");
     }
   }
 }
